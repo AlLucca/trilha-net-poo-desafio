63f4d8eaa6cdb72b07fa07c18e722f1f9050fc37
AlLucca <allima.devpro@gmail.com>
2024-12-01 01:23:25 -0300

    entrega do desafio Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123", "Nokia", "123", 32);
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ReceberLigacao();
+nokia.Ligar();
+
+Iphone iphone = new Iphone("123", "Iphone", "123", 32);
+iphone.InstalarAplicativo("WhatsApp");
+iphone.ReceberLigacao();
+iphone.Ligar();
